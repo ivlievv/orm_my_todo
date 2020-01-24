2846714adf98cd8ba9f2fe21b22491c63dfd2f7c
ivlievv <igor852745@gmail.com>
2020-01-24 17:02:46 +0200

    corrections commit

--- server/src/db/seeders/20200122130156-user.js
+++ server/src/db/seeders/20200122130156-user.js
@@ -1,35 +1,37 @@
 'use strict';
-
-const bcrypt = require('bcrypt');
+const bcrypt = require( 'bcrypt' );
 
 function generateUsers () {
   const users = [];
   for (let i = 0; i < 100;) {
-    users.push(new Object({
-                            firstName: 'Test',
-                            lastName: 'Testovich',
-                            login: `test_login${++i}`,
-                            email: `test${i}@gmail.com`,
-                            passwordHash: bcrypt.hashSync(`password${i}`, bcrypt.genSaltSync(10)),
-                            createdAt: new Date(),
-                            updatedAt: new Date(),
-                          }));
+    users.push( new Object( {
+                              firstName: 'Test',
+                              lastName: 'Testovich',
+                              login: `test_login${++i}`,
+                              email: `test${i}@gmail.com`,
+                              passwordHash: bcrypt.hashSync( `password${i}`, bcrypt.genSaltSync( 10 ) ),
+                              createdAt: new Date(),
+                              updatedAt: new Date(),
+                            } ) )
+    ;
   }
   return users;
+
 }
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', generateUsers(),);
+
+    return queryInterface.bulkInsert( 'Users', generateUsers(), {} );
   },
 
   down: (queryInterface, Sequelize) => {
     /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+     Add reverting commands here.
+     Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+     Example:  */
+    return queryInterface.bulkDelete('Users', null, {});
+
   }
 };
--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,9 @@
-import { User, sequelize } from './db/models';
-import moment from 'moment';
-import { DataTypes, Model } from 'sequelize';
+import {User} from './db/models';
 
 
-class Task extends Model {
-
+async function test () {
+  const usersId = await User.findAll( {
+                                        attributes: ['id'],
+                                      } );
+  console.log(usersId)
 }
-
-Task.init({
-            value: {
-              type: DataTypes.STRING,
-              allowNull: false,
-              validate: {
-                notEmpty: true,
-              }
-            },
-            deadline: {
-              type: DataTypes.DATE,
-              allowNull: false,
-              validate: {
-                isDate: true,
-                isAfter: moment().format('YYYY-MM-DD'),
-              }
-            },
-            isDone: {
-              type: DataTypes.BOOLEAN,
-              defaultValue: false,
-              allowNull: false
-            }
-          }, {
-            sequelize,
-          });
-
-Task.belongsTo(User);
-User.hasMany(Task);
-
-Task.sync();
